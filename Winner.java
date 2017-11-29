43dd7bd98472dc80091857e7daa8e5f1fbac064f
lottelelele <33837571+lottelelele@users.noreply.github.com>
2017-11-28 22:27:57 -0500

    Add files via upload

--- Winner.java
+++ Winner.java
@@ -31,9 +31,33 @@ public class Winner {
   @return return true if a player wins; return falls if nobody has won
   */
   public static boolean win(char currentPlayer, int currentR, int currentC){
-    return ((board[currentR][0] == currentPlayer)&&(board[currentR][1] == currentPlayer)&&(board[currentR][2] == currentPlayer))
-        || ((board[0][currentC] == currentPlayer)&&(board[1][currentC] == currentPlayer)&&(board[2][currentC] == currentPlayer))
-        || ((currentR== currentC)&&(board[0][0] == currentPlayer)&&(board[1][1] == currentPlayer)&&(board[2][2] == currentPlayer))
-        || ((currentR + currentC == 2)&&(board[0][2] == currentPlayer)&&(board[1][1] == currentPlayer)&&(board[2][0] == currentPlayer));
+    return ((board[currentR][0] == currentPlayer)&&(board[currentR][1] == currentPlayer)&&(board[currentR][2] == currentPlayer)) //if the current player gets three in a row
+        || ((board[0][currentC] == currentPlayer)&&(board[1][currentC] == currentPlayer)&&(board[2][currentC] == currentPlayer)) //if the current player gets three in a column
+        || ((currentR== currentC)&&(board[0][0] == currentPlayer)&&(board[1][1] == currentPlayer)&&(board[2][2] == currentPlayer)) //if the current player has three in a diagonal
+        || ((currentR + currentC == 2)&&(board[0][2] == currentPlayer)&&(board[1][1] == currentPlayer)&&(board[2][0] == currentPlayer)); //if the current player has three in the opposite diagonal
   }
+
+
+
+  /**
+  Check which player is the winner or noone is the winner.
+  @param currentR the number of row the current player puts X or O on
+  @param currentC the number of column the current player puts X or O on
+  */
+
+  public static void checkwinner(int currentR, int currentC){
+     currentPlayer=(char)(board[currentR][currentC]);
+     if (win(currentPlayer,currentR, currentC)){ //if we have a winner at this time
+       if(currentPlayer=='X'){ //the winner is the user
+         currentS="X";
+       }
+       else{ //the winner is the computer
+         currentS="O";
+       }
+     }
+     else if(tie()){ //if there is no winner at this time
+       currentS="T";
+     }
+   }
+
 }
