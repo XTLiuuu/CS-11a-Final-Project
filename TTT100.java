445b29e6f24cc0a11e3c145e085c811e7ff8252f
YijunYan <33837615+YijunYan@users.noreply.github.com>
2017-12-02 21:21:29 -0500

    player

--- TTT100.java
+++ TTT100.java
@@ -24,15 +24,45 @@ public class TTT100{
     first=TextIO.getlnBoolean(); //whether the user chooses to play first
     do{
       playTurn(); //the main game class of computer and user
-      if (currentS.equals("X")){ //"X" will be assigned to currentS when the user wins
-        System.out.println("You win!");} // print out the game result
-      else if (currentS.equals("O")){ //"O" will be assigned to currentS when the computer wins
-        System.out.println("I win!"); // print out the game result
-      }
-      else if (currentS.equals("T")){ //"T" will be assigned to currentS when it is a tie
-        System.out.println("This is a tie!"); //print out the game result
-      }
     }while(currentS.equals("P")); //currentS remains the initialize value when the game is in process
+  }
+
+  /**
+  prints out the result of the game
+  */
+  public static void printGameResult(){
+    if (currentS.equals("X")){ //"X" will be assigned to currentS when the user wins
+      System.out.println("You win!");} // print out the game result
+    else if (currentS.equals("O")){ //"O" will be assigned to currentS when the computer wins
+      System.out.println("I win!"); // print out the game result
+    }
+    else if (currentS.equals("T")){ //"T" will be assigned to currentS when it is a tie
+      System.out.println("This is a tie!"); //print out the game result
+    }
+  }
+
+  /**
+   an ordered sequence of moves that the computer would take if user go first
+   First, computer should check if the center cell is empty; if so, it should take the center cell
+   Second, computer should check if there are two '0's on the same row, column or diagonal; if so,
+   it should occupy the cell that would make three 'O's
+   Third, computer should check if there are two 'X's on the same row, column or diagonal; if so,
+   it should thake the remaining empty cell on that row, column or diagonal so that the user would not win
+   Fourth, computer should check if there are empty cells on the edge, i.e. cell [0][1],[1][0],[1][2],[2][1];
+   if so, it should take the first empty edge cell it finds
+   Finally, it should check if there still are empty cells on the board; if so, it should take the first
+   empty cell it finds.
+  */
+  public static void userGoFirst() {
+    if(!goCenter()){ //if the center cell is not empty
+      if(!Check.noughtsOrCross('O')){ //check if there are two 'O' on the same row, column or diagonal
+        if(!Check.noughtsOrCross('X')){ //check if there are two 'X' on the same row, column or diagonal
+          if(!Check.edge()){ //check if there are empty cells on the edge
+            Check.checkEmpty(); //the computer will make a move
+          }
+        }
+      }
+    }
   }
 
 
@@ -55,66 +85,73 @@ public class TTT100{
     }
   }
 
+
+  /**
+
+  */
+  public static void computerGoFirst {
+    if (i==1){ //the first move of the game (computer's first move)
+      moveV2(0,0,'O');//computer takes the upper left corner
+    } else if(i==3){ //the third move of the game (computer's second move)
+      computerGoFirstSecondMove();
+    } else if((i==5)&&(board[1][1]==' ')){ //corporate with i=3???????????
+      moveV2(1,1,'O');
+    } else{
+      computerGoFirstGeneralMove();
+    }
+  }
+
+
+  /**
+  computer's second move when it goes first
+  */
+  public static void computerGoFirstSecondMove {
+    if(board[1][1]=='X'){ //check whether the center cell is occupied by the user (the user's first move)
+      moveV2(2,2,'O');// computer takes the center
+    } else{
+      //if the center cell is still empty
+      //the computer avoids occupying a cell on the same row of the computer's first move
+      if(currentR==0){ //if the user occupies a cell on the same row of the computer's first move
+        moveV2(2,0,'O'); //the computer takes the [2][0]
+      } else{ //if the user does not occupy a cell on the same row of the computer's first move
+        moveV2(0,2,'O'); //the computer takes the [0][2]
+      }
+    }
+  }
+
+  /**
+  computer occupies a specific cell
+  */
+  public static void moveV2 (int a, int b, char symbol){
+    board[a][b]=symbol; //computer occupies this cell
+    currentR=a; //set the current row number as currentR; updates currentR to a
+    currentC=b; //set the current column number as currentC; updates currentC to b
+  }
+
+  /**
+  */
+  public static void computerGoFirstGeneralMove {
+    if(!Check.noughtsOrCross('O')){
+      if(!Check.noughtsOrCross('X')){
+        if(Check.corner()==false){
+          Check.checkEmpty();
+        }
+      }
+    }
+  }
+
   /**
   generates the computer's move
   */
 
   public static void computer(){
-    System.out.println("Now it's my turn");
-    System.out.println(" ");
+    System.out.println("Now it's my turn\n");
     if(i%2==0){ //when user goes first, the move number of the computer is even
-      if(!Check.goCenter()){ //if the center cell is not empty
-        if(!Check.noughtsOrCross('O')){ //check if there are two 'O' on the same row, column or diagonal
-          if(!Check.noughtsOrCross('X')){ //check if there are two 'X' on the same row, column or diagonal
-            if(!Check.Edge()){ //check if there are empty cells on the edge
-              Check.checkEmpty(); //the computer will make a move
-            }
-          }
-        }
-      }
+      userGoFirst();
     }
     //decide the computer's next move based on the user's move
     if (i%2!=0){ //when computer goes first, the move number of the computer is odd
-      if(i==1){ //the first move of the game (made by computer)
-        board[0][0]='O'; //computer will take the corner
-        currentR=0; //set the current row number to the currentR, which is 0
-        currentC=0; //set the current column number to the currentC, which is 0
-      }
-      else if(i==3){ //the third move of the game (made by computer)
-        if(board[1][1]=='X'){ //check whether the center cell is occupied by the user (the user's first move)
-          board[2][2]='O'; //the computer's move
-          currentR=2; //set current row number to currnetR
-          currentC=2; //set current column number to currnetC
-        }
-        else{
-          //if the center cell is still empty
-          //the computer avoids occupying a cell on the same row of the computer's first move
-          if(currentR==0){ //if the user occupies a cell on the same row of the computer's first move
-            board[2][0]='O'; //the computer takes the [2][0]
-            currentR=2;
-            currentC=0;
-          }
-          else{ //if the user does not occupy a cell on the same row of the computer's first move
-            board[0][2]='O'; //the computer takes the [0][2]
-            currentR=0;
-            currentC=2;
-          }
-        }
-      }
-      else if((i==5)&&(board[1][1]==' ')){ //corporate with i=3???????????
-        board[1][1]='O';
-        currentR=0;
-        currentC=0;
-      }
-      else{
-        if(!Check.noughtsOrCross('O')){
-          if(!Check.noughtsOrCross('X')){
-            if(Check.corner()==false){
-              Check.checkEmpty();
-            }
-          }
-        }
-      }
+      computerGoFirst();
     }
     Winner.checkWinner(currentR,currentC);  //check if there is a winner or a tie or still in process
   }
@@ -129,16 +166,12 @@ public class TTT100{
     int row=input.nextInt()-1; //turn the format of row into the right version that the computer can read ?
     int column=input.nextInt()-1; //turn the format of column into the right version that the computer can read ?
     if ((row >= 0)&&(row < 3)&&(column>= 0)&&(column< 3)&&(board[row][column]==' ')){ //check if the user's move is valid (whether the cell is empty)
-      board[row][column]='X'; //input the user's move
-      currentR=row;
-      currentC=column;
+      moveV2(row,column,'X'); //input the user's move
       Board.printBoard(); //print the board after this move
       first=false; //change the next player to computer
-    }
-    else{ //if the user's move is not valid
+    } else{ //if the user's move is not valid
       System.out.println("This move is not valid. Please try again");
       player(); //ask the user to input again
    }
  }
-
 }
