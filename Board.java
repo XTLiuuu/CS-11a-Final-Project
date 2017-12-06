2eaa11045efc287f85eb21e6295154701be9d948
XTLiuuu <33227289+XTLiuuu@users.noreply.github.com>
2017-12-05 20:41:03 -0500

    Add files via upload

--- Board.java
+++ Board.java
@@ -10,12 +10,10 @@ This contains a library of useful operations of the board
 public class Board{
 
   /**
-  This shows how to use the board.Initialize class to set the board up
-  before the game start
-  @param args an array of Strings
+  intialize() sets up an empty 3x3 board by assigning ' ' to every row and column
+  before a new game starts
   */
-
-  public static void initialize(String[] args){
+  public static void initialize(){
     for(int row=0; row<3; row++){
       for (int column=0; column<3; column++){ //go through every rows and columns
         TTT100.board[row][column]=' '; //every space is empty
@@ -26,15 +24,14 @@ public class Board{
 
 
   /**
-  This shows how to use the board.Move class to go to the position where
-  the user want to place next move
-  @param a an int
-  @param b an int
-  @param check an boolean varibale
-  @return the boolean expression which is always ture
+  move(a,b,check) places the user's or computer's move on the board
+  @param a an integer of the row number
+  @param b an integer of the column number
+  @param check an boolean varibale whose initial value is true
+  @return a boolean expression that is always "true"
   */
 
-  public static boolean Move(int a, int b, boolean check){
+  public static boolean move(int a, int b, boolean check){
     TTT100.board[a][b]='O';
     TTT100.currentR=a;
     TTT100.currentC=b;
@@ -43,18 +40,15 @@ public class Board{
 
 
   /**
-  This shows how to use the board.printboard class to print out the board
-  at each step
-  @param args an array of Strings
+  printBoard() prints out the current board after each moves
   */
 
   public static void printBoard(){
-    System.out.println();
-    System.out.println(TTT100.board[0][0]+" | "+TTT100.board[0][1]+" | "+TTT100.board[0][2]);
-    System.out.println("---------");
-    System.out.println(TTT100.board[1][0]+" | "+TTT100.board[1][1]+" | "+TTT100.board[1][2]);
-    System.out.println("---------");
-    System.out.println(TTT100.board[2][0]+" | "+TTT100.board[2][1]+" | "+TTT100.board[2][2]);
-    System.out.println();
+    System.out.println("   1  2   3");
+    System.out.println("1  " + TTT100.board[0][0] + "| " + TTT100.board[0][1] + " | " + TTT100.board[0][2]);
+    System.out.println("  -----------");
+    System.out.println("2  " + TTT100.board[1][0] + "| " + TTT100.board[1][1] + " | " + TTT100.board[1][2]);
+    System.out.println("  -----------");
+    System.out.println("3  " + TTT100.board[2][0] + "| " + TTT100.board[2][1] + " | " + TTT100.board[2][2]);
   }
 }
--- Check.java
+++ Check.java
@@ -3,50 +3,50 @@ package TicTacToe;
 public class Check{
 
   /**
-  check if the center cell is empty; if yes, let the computer take that cell
+  goCenter() checks if the center cell is empty; if yes, let the computer take that cell
   @return true if the center cell is empty and the computer takes that cell; false if the center cell is already occupied
   */
   public static boolean goCenter(){
     boolean check = true; // initialize check as true
     if(TTT100.board[1][1]==' '){ // check if the center cell is empty
-      return check=Board.Move(1,1,check); // return true; and computer occupies the center cell
+      return check=Board.move(1,1,check); // return true; and computer occupies the center cell
     } else {
       return false; // return false if the center cell is already occupied
     }
   }
 
-
   /**
-  check if there are empty cells on the edges (i.e. cell [0][1],[1][0],[1][2],[2][1])
+  edge() checks if there are empty cells on the edges (i.e. cell [0][1],[1][0],[1][2],[2][1])
   @return true if there are empty cells on the edges
   */
   public static boolean edge(){
     boolean check=true; // intialize check as true
     // return true if computer occupies one of cell0][1], cell[1][0], cell[1][2], cell[2][1]; otherwise, false
-    return check=checkEdge(check);
+    return checkEdge(check);
   }
 
   /**
-  check if one of cell0][1], cell[1][0], cell[1][2], cell[2][1] is empty; if yes, the computer occupies that cell
+  checkEdge(check) checks if one of cell0][1], cell[1][0], cell[1][2], cell[2][1] is empty; if yes, the computer occupies that cell
   @param check the initail value is true
   @return true if one of cell0][1], cell[1][0], cell[1][2], cell[2][1] is empty; otherwise, false
   */
+
   public static boolean checkEdge(boolean check){
     if(TTT100.board[0][1]==' '){ // check if cell[0][1] is empty
-      return Board.Move(0,1,check); // return true; and computer occupies cell [0][1]
+      return Board.move(0,1,check); // return true; and computer occupies cell [0][1]
     } else if(TTT100.board[1][0]==' '){ // check if cell[1][0] is empty
-      return Board.Move(1,0,check); // return true; and computer occupies cell [1][0]
+      return Board.move(1,0,check); // return true; and computer occupies cell [1][0]
     } else if(TTT100.board[1][2]==' '){ // check if cell[1][2] is empty
-      return Board.Move(1,2,check); // return true; and computer occupies cell [1][2]
+      return Board.move(1,2,check); // return true; and computer occupies cell [1][2]
     } else if(TTT100.board[2][1]==' '){ // check if cell[2][1] is empty
-      return Board.Move(2,1,check); // return true; and computer occupies cell [2][1]
+      return Board.move(2,1,check); // return true; and computer occupies cell [2][1]
     } else{
       return false; // return false if none of cell0][1], cell[1][0], cell[1][2], cell[2][1] is empty
     }
   }
 
   /**
-  check if there is empty cell on the corner
+  corner() checks if there is empty cell on the corner
   @return true there is empty cell on the corners
   */
   public static boolean corner(){
@@ -55,27 +55,27 @@ public class Check{
   }
 
   /**
-  returns the the move of computer player to one corner of the board if the cell
+  checkCorner(check) lets the computer takes one corner of the board if the cell
   is still empty at this time
   @param check initail value is true
   @return true if computer moves to an empty cell in the corner; otherwise, false
   */
   public static boolean checkCorner(boolean check){
     if(TTT100.board[0][0]==' '){ // check if cell[0][0] is empty
-      return Board.Move(0,0,check); // return true; and computer occupies cell [0][0]
+      return Board.move(0,0,check); // return true; and computer occupies cell [0][0]
     } else if(TTT100.board[0][2]==' '){ // check if cell[0][2] is empty
-      return Board.Move(0,2,check); // return true; and computer occupies cell [0][2]
+      return Board.move(0,2,check); // return true; and computer occupies cell [0][2]
     } else if(TTT100.board[2][2]==' '){ // check if cell[2][2] is empty
-      return Board.Move(2,2,check); // return true; and computer occupies cell [2][2]
+      return Board.move(2,2,check); // return true; and computer occupies cell [2][2]
     } else if(TTT100.board[2][0]==' '){ // check if cell[2][0] is empty
-      return Board.Move(2,0,check); // return true; and computer occupies cell [2][0]
+      return Board.move(2,0,check); // return true; and computer occupies cell [2][0]
     } else{
       return false; // return false if none of the corner cells is empty
     }
   }
 
   /**
-  Finds the empty cells in this 3*3 board and let computer take the first
+  checkEmpty() finds the empty cells in this 3*3 board and let computer take the first
   empty cell it finds
   */
   public static void checkEmpty(){
@@ -93,7 +93,7 @@ public class Check{
   }
 
   /**
-  Check if there are two noughts or crosses on a single row, column or diagonal
+  noughtsOrCross(symbol) checks if there are two noughts or crosses on a single row, column or diagonal
   @param symbol 'X' or 'O'
   @return true if there are two noughts or crosses in a single row, column or diagonal; otherwise, false
   */
@@ -112,7 +112,7 @@ public class Check{
   }
 
   /**
-  Check if there are two noughts or two crosses on a single row
+  row(symbol) checks if there are two noughts or two crosses on a single row
   @param symbol 'X' or 'O'
   @return true if there are two noughts or two crosses on a single row; otherwise, false
   */
@@ -123,7 +123,7 @@ public class Check{
 
 
   /**
-  Check if there are two noughts or two crosses on row 0,1,or 2
+  checkRow(check,symbol) checks if there are two noughts or two crosses on row 0,1,or 2
   @param check initial value is true
   @param symbol 'X' or 'O'
   @return true if there are two noughts or two crosses on row 0,1,or 2; otherwise, false
@@ -145,7 +145,7 @@ public class Check{
   }
 
   /**
-  checks if a row, column, or diagonal has two cells occupied by the same symbol('X' or 'O')
+  twoOccupied(a,b,c,d,e,f,check,symbol) checks if a row, column, or diagonal has two cells occupied by the same symbol('X' or 'O')
   the computer will occupy the remaining cell on that row, column, or diagonal if it's empty
   @param a row number of cell 1
   @param b column number of cell 1
@@ -159,14 +159,14 @@ public class Check{
   */
   public static boolean twoOccupied(int a, int b, int c,int d,int e,int f,boolean check,char symbol){
     if(TTT100.board[a][b]==symbol && TTT100.board[c][d]==symbol && empty(e,f)) {
-      return Board.Move(e,f,check);
+      return Board.move(e,f,check);
     } else {
       return false;
     }
   }
 
   /**
-  Check if there are two noughts or two crosses on a single column
+  col(symbol) checks if there are two noughts or two crosses on a single column
   @param symbol 'X' or 'O'
   @return true if there are two noughts or two crosses on a single column; otherwise, false
   */
@@ -176,7 +176,7 @@ public class Check{
   }
 
   /**
-  Check if there are two noughts or two crosses on column 0,1,or 2
+  checkCol(check,symbol) checks if there are two noughts or two crosses on column 0,1,or 2
   @param check initial value is true
   @param symbol 'X' or 'O'
   @return true if there are two noughts or two crosses on column 0,1,or 2; otherwise, false
@@ -198,7 +198,7 @@ public class Check{
   }
 
   /**
-  Check if there are two noughts or two crosses on the diagonal from left to right
+  checkDiagonal(symbol) checks if there are two noughts or two crosses on the diagonal from left to right
   @param symbol 'X' or 'O'
   @return true if there are two noughts or two crosses on the diagonal from left to right; otherwise, false
   */
@@ -218,7 +218,7 @@ public class Check{
   }
 
   /**
-  Check if there are two noughts or two crosses on the diagonal from right to left
+  checkOppositeDiagonal(symbol) checks if there are two noughts or two crosses on the diagonal from right to left
   @param symbol 'X' or 'O'
   @return true if there are two noughts or two crosses on the diagonal from right to left; otherwise, false
   */
@@ -239,8 +239,7 @@ public class Check{
 
 
   /**
-    checks whether a certain cell is empty
-    passed in as an integer row and an integer col
+    empty(row,col) checks whether a certain cell is empty
     @param row the row number of this cell
     @param col the column number of this cell
     @return true if the cell is empty, false if the cell is occupied; otherwise, false
